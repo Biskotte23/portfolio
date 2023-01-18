50161e49832dcb1bc63d0575311f8331fbaedc44
Jules Pascot <jules.pascot@gmail.com>
2023-01-18 09:26:46 +0100

    chore: initialization of the project

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import styles from "./App.module.scss";
+
+export default function App() {
+  return (
+    <div className={styles.portfolio}>
+      <h1>Jules PASCOT - Portfolio</h1>
+    </div>
+  );
+}
